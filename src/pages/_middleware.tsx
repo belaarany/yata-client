0a1c862197849b2bccd9bfe1a4099549c47e3862
Bela Arany <git@belaarany.com>
2022-04-25 08:44:50 +0200

    fix

--- src/pages/_middleware.tsx
+++ src/pages/_middleware.tsx
@@ -1,8 +1,10 @@
 import { NextResponse, NextRequest } from "next/server"
-export async function middleware(req: any, ev: any) {
-	const { pathname } = req.nextUrl
-	if (pathname == "/") {
-		return NextResponse.redirect("/inbox")
+export async function middleware(req: NextRequest) {
+	const url = req.nextUrl.clone()
+
+	if (url.pathname == "/") {
+		url.pathname = "/inbox"
+		return NextResponse.redirect(url)
 	}
 	return NextResponse.next()
 }
